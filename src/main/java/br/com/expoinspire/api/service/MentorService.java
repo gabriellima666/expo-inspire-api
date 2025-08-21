d538224d0cd1ea54caf6a8ba86b27bcf8d46f186
gabriellima666 <gabriellimadesousa0@gmail.com>
2025-08-21 15:47:32 -0300

    Controller mentor

--- src/main/java/br/com/expoinspire/api/service/MentorService.java
+++ src/main/java/br/com/expoinspire/api/service/MentorService.java
@@ -2,6 +2,7 @@ package br.com.expoinspire.api.service;
 
 import br.com.expoinspire.api.entity.Mentor;
 import br.com.expoinspire.api.repository.MentorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @Service
 public class MentorService {
+    @Autowired
     private MentorRepository mentorRepository;
 
     public List<Mentor> getAll () {
